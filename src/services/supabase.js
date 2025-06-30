ce486ce34f4d9ea1e589187f32998e87eb5e4527
Samuel-Tefera <samuelteferab@gmail.com>
2025-06-30 12:29:14 +0300

    export supabase url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://tcprgupktzbbsbycgqbw.supabase.co';
+export const supabaseUrl = 'https://tcprgupktzbbsbycgqbw.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcHJndXBrdHpiYnNieWNncWJ3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTA0NDU1MiwiZXhwIjoyMDY2NjIwNTUyfQ.aDFWueeO2iHX23vn-4f3hU5TS_TDU3FYxnNlTwpb79M';
 const supabase = createClient(supabaseUrl, supabaseKey);
