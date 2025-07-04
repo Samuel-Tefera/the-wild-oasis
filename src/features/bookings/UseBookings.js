d3ee03d0bb04351fdb4b4fe3ce25b008f1631b9c
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-04 14:25:00 +0300

    add filter by status and sort bookings operation

--- src/features/bookings/UseBookings.js
+++ src/features/bookings/UseBookings.js
@@ -1,10 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterName = searchParams.get('status');
+  const filter =
+    filterName === 'all' || !filterName
+      ? null
+      : { field: 'status', value: filterName };
+
+  // SORT
+  const sortByRow = searchParams.get('sortBy');
+  const [field, direction] = sortByRow?.split('-') || [];
+
+  const sortBy = sortByRow ? { field, direction } : null;
+
   const { isLoading, data: bookings } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings(filter, sortBy),
   });
 
   return { isLoading, bookings };
