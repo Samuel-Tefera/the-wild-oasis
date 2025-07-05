21fafc7f798b4061153ce6c2ca4fc15d8b5f541f
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-05 21:03:06 +0300

    add route to page booking detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 import Dashboard from './pages/Dashboard';
-import Booking from './pages/Bookings';
 import Cabins from './pages/Cabins';
 import Account from './pages/Account';
 import Login from './pages/Login';
@@ -12,8 +13,8 @@ import Settings from './pages/Settings';
 import Users from './pages/Users';
 import AppLayout from './ui/AppLayout';
 import GlobalStyles from './styles/GlobalStyles';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'react-hot-toast';
+import Bookings from './pages/Bookings';
+import Booking from './pages/Booking';
 
 const queryClinet = new QueryClient({
   defaultOptions: {
@@ -38,7 +39,8 @@ export default function App() {
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
-            <Route path="booking" element={<Booking />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
