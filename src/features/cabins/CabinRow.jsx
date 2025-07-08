100a63377748a9365904fb430ae1a354eafb3795
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-08 13:34:19 +0300

    disable button when duplicate cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -76,7 +76,11 @@ export default function CabinRow({ cabin }) {
             <Menus.Toggle id={cabinId} />
 
             <Menus.List id={cabinId}>
-              <Menus.Button onClick={handleDuplicate} icon={<HiSquare2Stack />}>
+              <Menus.Button
+                onClick={handleDuplicate}
+                icon={<HiSquare2Stack />}
+                disabled={isCreating}
+              >
                 Duplicate
               </Menus.Button>
 
