325cc5c9c7c1623e43c3c74f9179867e4d333e22
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-11 21:51:36 +0300

    hook to fetch guest stays

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+import { getStaysAfterDate } from '../../services/apiBookings';
+import { subDays } from 'date-fns';
+
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get('last') ? 7 : +searchParams.get('last');
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ['stays', `last-${numDays}`],
+  });
+
+  return { isLoading, stays };
+}
